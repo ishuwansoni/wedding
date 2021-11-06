ce3c0d10ffd33dba454d83bc40772dac872f8316
ishuwansoni <93809222+ishuwansoni@users.noreply.github.com>
2021-11-06 11:04:18 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,14 +10,17 @@
         ],                   // Horizontal movement animation names
         className: 'sakura', // Class name to use
         fallSpeed: 2.5,        // Factor for petal fall speed
-        maxSize: 12,         // Maximum petal size
+        maxSize: 15,         // Maximum petal size
         minSize: 9,          // Minimum petal size
         newOn: 250,          // Interval after which a new petal is added
         
     });
 
 })(jQuery);
-
+$(document).ready( function(){
+    $("#my_audio").get(0).play();
+    console.log('Shaadi me zaroor aana');
+});
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
@@ -30,7 +33,7 @@ function pauseAudio() {
 };
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 28, 2021 19:00:00").getTime();
+var countDownDate = new Date("Nov 28, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -49,8 +52,8 @@ var x = setInterval(function() {
     
     // Output the result in an element with id="demo"
     document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
-    
-    // If the count down is over, write some text 
+
+    // If the count down is over, write some text
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("time").innerHTML = "Wedding Completed !!";
@@ -61,18 +64,18 @@ var x = setInterval(function() {
 }, 1000);
 
 // being a bit cool :p  
-var styles = [
-    'background: linear-gradient(#D33106, #571402)'
-    , 'border: 4px solid #3E0E02'
-    , 'color: white'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
-    , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
-    , 'line-height: 40px'
-    , 'text-align: center'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
-].join(';');
+//var styles = [
+//    'background: linear-gradient(#D33106, #571402)'
+//    , 'border: 4px solid #3E0E02'
+//    , 'color: white'
+//    , 'display: block'
+//    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
+//    , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
+//    , 'line-height: 40px'
+//    , 'text-align: center'
+//    , 'font-weight: bold'
+//    , 'font-size: 32px'
+//].join(';');
 
 var styles1 = [
     'color: #FF6C37'
@@ -92,7 +95,7 @@ var styles2 = [
     , 'font-size: 32px'
 ].join(';');
 
-console.log('\n\n%c SAVE THE DATE: 14th Feb, 2021', styles);
+console.log('\n\n%c SAVE THE DATE: 28th Nov, 2021', styles);
 
 console.log('%cYour presence is requested!%c\n\nRegards: Sarthak Jain', styles1, styles2);
 
