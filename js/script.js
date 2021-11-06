2aa290a80d96095ee98fd5eb95652e222f44ae6c
ishuwansoni <93809222+ishuwansoni@users.noreply.github.com>
2021-11-06 11:09:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function pauseAudio() {
 };
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 28, 2021 00:19:00").getTime();
+var countDownDate = new Date("Nov 28, 2021 19:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
